491a81daa14fc6df8ba70c83a355c2af74a27380
Sipo Charles <sipocharles@yahoo.co.uk>
2020-11-23 19:18:06 +0000

    Articles App + Edit/Delete

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # Local
     'accounts',
     'pages',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/London'
 
 TIME_ZONE = 'UTC'
 
